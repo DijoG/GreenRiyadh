2b48f3a2d984f4c2ea5e4b7c400d3f7fe375c88d
DijoG <34399619+DijoG@users.noreply.github.com>
2025-05-24 08:01:26 +0200

    Update Arabic_RUN_BEAUTIFYinBEAUTIFILL.R

--- Arabic_RUN_BEAUTIFYinBEAUTIFILL.R
+++ Arabic_RUN_BEAUTIFYinBEAUTIFILL.R
@@ -1,4 +1,4 @@
 ###
-source("D:/TMO/R/BEAUTIFYinBEAUTIFILL.R")
+source(".../R/BEAUTIFYinBEAUTIFILL.R")
 ### Example:
 BEAUTIFILL(".../03 Planning/1232-T2-TM2_1-GIS-Remote-Sensing/01_Correspondence/Incoming/Fulcrum_Exl/BRT_SDA_Road")
